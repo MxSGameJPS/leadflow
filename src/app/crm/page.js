b63117c832bf82644ef39b876d6272519a268587
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-07-29 18:39:26 -0300

    feat: use compact CRM board

--- src/app/crm/page.js
+++ src/app/crm/page.js
@@ -1,9 +1,9 @@
 import { listLeads } from "../../repositories/leadRepository.js";
-import Board from "../../components/Board/Board.jsx";
+import CRMBoard from "../../components/CRMBoard/CRMBoard.jsx";
 
 export const dynamic = "force-dynamic";
 
 export default async function CRMPage() {
   const leads = await listLeads();
-  return <Board initialLeads={leads} />;
+  return <CRMBoard initialLeads={leads} />;
 }
