94087441d9e3b3fc2c2ece516e251a78de8525e0
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-07-30 06:13:57 -0300

    feat: load professional profile in lead workspace

--- src/app/crm/[id]/page.js
+++ src/app/crm/[id]/page.js
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { getLead } from "../../../repositories/leadRepository.js";
 import { getLeadWorkspace } from "../../../services/workspaces/leadWorkspaceStore.js";
+import { getProfessionalProfile } from "../../../services/profile/profileStore.js";
 import LeadWorkspace from "../../../components/LeadWorkspace/LeadWorkspace.jsx";
 
 export const dynamic = "force-dynamic";
@@ -10,6 +11,9 @@ export default async function LeadDetailPage({ params }) {
   const lead = await getLead(id);
   if (!lead) notFound();
 
-  const workspace = await getLeadWorkspace(lead.id);
-  return <LeadWorkspace initialLead={lead} initialWorkspace={workspace} />;
+  const [workspace, profile] = await Promise.all([
+    getLeadWorkspace(lead.id),
+    getProfessionalProfile(),
+  ]);
+  return <LeadWorkspace initialLead={lead} initialWorkspace={workspace} initialProfile={profile} />;
 }
