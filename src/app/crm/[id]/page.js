817104e66ec8a1b364efdff68ab827eb80aab26a
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-07-31 18:41:26 -0300

    feat: expose opportunity type in CRM detail

--- src/app/crm/[id]/page.js
+++ src/app/crm/[id]/page.js
@@ -2,6 +2,8 @@ import { notFound } from "next/navigation";
 import { getLead } from "../../../repositories/leadRepository.js";
 import { getLeadWorkspace } from "../../../services/workspaces/leadWorkspaceStore.js";
 import { getProfessionalProfile } from "../../../services/profile/profileStore.js";
+import { resolveCommercialTrack } from "../../../services/leads/commercialTrack.js";
+import CommercialTrackSelector from "../../../components/CommercialTrackSelector/CommercialTrackSelector.jsx";
 import LeadWorkspace from "../../../components/LeadWorkspace/LeadWorkspace.jsx";
 
 export const dynamic = "force-dynamic";
@@ -15,5 +17,10 @@ export default async function LeadDetailPage({ params }) {
     getLeadWorkspace(lead.id),
     getProfessionalProfile(),
   ]);
-  return <LeadWorkspace initialLead={lead} initialWorkspace={workspace} initialProfile={profile} />;
+  const commercialTrack = resolveCommercialTrack(lead, workspace);
+
+  return <>
+    <CommercialTrackSelector leadId={lead.id} grade={lead.grade} initialTrack={commercialTrack} />
+    <LeadWorkspace initialLead={lead} initialWorkspace={{ ...workspace, commercialTrack }} initialProfile={profile} />
+  </>;
 }
