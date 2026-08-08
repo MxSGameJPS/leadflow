671608b4bd76651bd327054d67cfe7948caf66bd
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-08-08 10:51:25 -0300

    feat: carregar ultimo contato no CRM

--- src/app/crm/page.js
+++ src/app/crm/page.js
@@ -1,4 +1,5 @@
 import { listLeads } from "../../repositories/leadRepository.js";
+import { listContactStates } from "../../repositories/contactTrackingRepository.js";
 import { getLeadWorkspace } from "../../services/workspaces/leadWorkspaceStore.js";
 import { resolveCommercialTrack, trackIncludes } from "../../services/leads/commercialTrack.js";
 import CRMBoard from "../../components/CRMBoard/CRMBoard.jsx";
@@ -6,10 +7,15 @@ import CRMBoard from "../../components/CRMBoard/CRMBoard.jsx";
 export const dynamic = "force-dynamic";
 
 export default async function CRMPage() {
-  const leads = await listLeads();
+  const [leads, contactStates] = await Promise.all([listLeads(), listContactStates()]);
+  const contactsByLead = new Map(contactStates.map(state => [state.id, state]));
   const workspaces = await Promise.all(leads.map(lead => getLeadWorkspace(lead.id)));
   const projectLeads = leads
-    .map((lead, index) => ({ ...lead, commercialTrack: resolveCommercialTrack(lead, workspaces[index]) }))
+    .map((lead, index) => ({
+      ...lead,
+      ...(contactsByLead.get(lead.id) || {}),
+      commercialTrack: resolveCommercialTrack(lead, workspaces[index]),
+    }))
     .filter(lead => trackIncludes(lead.commercialTrack, "projects"));
   return <CRMBoard initialLeads={projectLeads} />;
 }
