dd0a359efce50a7ef72069f304534c63a4acdc0a
Saulo Pavanello <153878745+MxSGameJPS@users.noreply.github.com>
2026-08-08 11:18:39 -0300

    feat: carregar ultimo contato no crm

--- src/app/crm/page.js
+++ src/app/crm/page.js
@@ -9,7 +9,13 @@ export default async function CRMPage() {
   const leads = await listLeads();
   const workspaces = await Promise.all(leads.map(lead => getLeadWorkspace(lead.id)));
   const projectLeads = leads
-    .map((lead, index) => ({ ...lead, commercialTrack: resolveCommercialTrack(lead, workspaces[index]) }))
+    .map((lead, index) => ({
+      ...lead,
+      lastContactAt: workspaces[index].lastContactAt || "",
+      lastContactKind: workspaces[index].lastContactKind || "",
+      contactCount: Number(workspaces[index].contactCount || 0),
+      commercialTrack: resolveCommercialTrack(lead, workspaces[index]),
+    }))
     .filter(lead => trackIncludes(lead.commercialTrack, "projects"));
   return <CRMBoard initialLeads={projectLeads} />;
 }
